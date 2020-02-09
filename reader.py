03158b6cf3a4d30d760d7147aa6d732452fb6cab
Jan Ruzicka <jan.ruzicka.1@plus4u.net>
2020-02-09 12:00:46 +0100

    Added prolongement of the lines. Fixed direction of the first line.

--- reader.py
+++ reader.py
@@ -1,7 +1,8 @@
 import os
 import math
 
-from qgis.core import QgsGeometry, QgsPointXY, QgsCoordinateTransform, QgsCoordinateReferenceSystem, QgsProject
+from qgis.core import QgsGeometry, QgsLineString, QgsPointXY, QgsPoint, \
+    QgsCoordinateTransform, QgsCoordinateReferenceSystem, QgsProject
 
 class AerogenReaderError(Exception):
     pass
@@ -80,11 +81,9 @@ class AerogenReader(object):
         return [QgsGeometry.fromPolygonXY([self._polygon_points])]
 
     def sl(self):
-        print("SL", self._basename, self._dirname)
         return [self._get_lines('sl')]
 
     def tl(self):
-        print("TL", self._basename, self._dirname)
         return [self._get_lines('tl')]
 
     def _get_id(self, line):
@@ -101,6 +100,94 @@ class AerogenReader(object):
         """Returns point with key as an id from the file."""
         items = line.split()
         return {items[4]: [items[2], items[3]]}
+
+    def _get_azimuth_diff(self, pt1, pt2, pt3):
+        """Returns difference between azimuths of two lines (pt1-pt2 and pt2-pt3)"""
+        first_segment_azimuth = pt1.azimuth(pt2)
+        if first_segment_azimuth < 0:
+            first_segment_azimuth += 360
+        second_segment_azimuth = pt2.azimuth(pt3)
+        if second_segment_azimuth < 0:
+            second_segment_azimuth += 360
+        diff = first_segment_azimuth - second_segment_azimuth
+        return diff
+
+    def _convert_to_crs(self, line_points):
+        """Converts line_points into UTM"""
+        crs_src = QgsCoordinateReferenceSystem(4326)
+        crs_dest = QgsCoordinateReferenceSystem(self.crs())
+        xform = QgsCoordinateTransform(crs_src, crs_dest, QgsProject.instance())
+        for i in range(len(line_points)):
+            line_points[i] = xform.transform(line_points[i])
+        return line_points
+
+    def _convert_to_wgs(self, line_points):
+        """Converts line_points into WGS84"""
+        crs_src = QgsCoordinateReferenceSystem(self.crs())
+        crs_dest = QgsCoordinateReferenceSystem(4326)
+        xform = QgsCoordinateTransform(crs_src, crs_dest, QgsProject.instance())
+        for i in range(len(line_points)):
+            line_points[i] = xform.transform(line_points[i])
+        return line_points
+
+    def _correct_first_segment(self, line_points):
+        """Switch first two points if the connection is not in good angle (close to normal).
+        It usually means that we took the first line in a wrong dirrection.
+        """
+        diff = self._get_azimuth_diff(line_points[0], line_points[1], line_points[2])
+        if (math.fabs(diff) > 70 and math.fabs(diff) < 110) \
+                or (math.fabs(diff) > 250 and math.fabs(diff) < 290):
+            # If the angle is about normal / 90 degrees, we do not do anything
+            return line_points
+        else:
+            if line_points[0].distance(line_points[1]) < (line_points[2].distance(line_points[3]) / 2):
+                pt0 = line_points[0]
+                line_points[0] = line_points[1]
+                line_points[1] = pt0
+            return line_points
+
+    def _correct_connections(self, line_points):
+        """We prolong the lines in a case when the connection is not in normal angle"""
+        i = 0
+        previous_diff = 90
+        while i < (len(line_points) - 3):
+            diff = self._get_azimuth_diff(line_points[i], line_points[i+1], line_points[i+2])
+            if not ((math.fabs(diff) > 85 and math.fabs(diff) < 95) or (math.fabs(diff) > 265 and math.fabs(diff) < 275)):
+                # The angle is not close to normal
+                if i == 0:
+                    # We are at the beginning and do not have previous diff, so we read next diff
+                    # TODO possible change to detect diff according to engles of the area, better results
+                    previous_diff = self._get_azimuth_diff(line_points[2], line_points[3], line_points[4])
+                distance_current = line_points[i].distance(line_points[i+1])
+                distance_next = line_points[i+2].distance(line_points[i+3])
+                line_g = QgsGeometry.fromPolylineXY([line_points[i + 1], line_points[i + 2]])
+                if distance_current > distance_next:
+                    # we go from longer to shorter line
+                    line = QgsLineString(QgsPoint(line_points[i+2]), QgsPoint(line_points[i+3]))
+                    line.extend(distance_current, 0)
+                    # We rotate the line segment to find cross with extended line
+                    # The rotation is based on diff (rotate to be along extended line)
+                    # and angle of the previous normal line
+                    line_g.rotate(diff + (180 - previous_diff), line_points[i + 1])
+                    line = QgsGeometry.fromWkt(line.asWkt())
+                    intersection = line_g.intersection(line).centroid()
+                    if not intersection.isNull():
+                        intersection = intersection.asPoint()
+                        line_points[i + 2] = intersection
+                else:
+                    # we go from shorter to longer line
+                    line = QgsLineString(QgsPoint(line_points[i]), QgsPoint(line_points[i+1]))
+                    line.extend(0, distance_next)
+                    line_g.rotate(diff + (180 - previous_diff), line_points[i + 2])
+                    line = QgsGeometry.fromWkt(line.asWkt())
+                    intersection = line_g.intersection(line).centroid()
+                    if not intersection.isNull():
+                        intersection = intersection.asPoint()
+                        line_points[i + 1] = intersection
+            i+=2
+            previous_diff = diff
+
+        return line_points
 
     def _get_lines(self, type):
         # Open the file with read only permit
@@ -130,6 +217,10 @@ class AerogenReader(object):
                 points.update(point)
         for key in sorted(points.keys()):
             line_points.append(self._build_point(points[key][0], points[key][1]))
+        line_points = self._convert_to_crs(line_points)
+        line_points = self._correct_first_segment(line_points)
+        line_points = self._correct_connections(line_points)
+        line_points = self._convert_to_wgs(line_points)
         return QgsGeometry.fromPolylineXY(line_points)
 
     def crs(self):
